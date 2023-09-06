cd2ed928735f4537c933bee21cd215152c9fa07a
Timothy Wood <codearachnid@gmail.com>
2023-09-06 17:42:25 -0400

    Create wpnexus-jwt-auth.php

---
+++ wpnexus-jwt-auth.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: JWT Auth
+ * Plugin URI:  https://github.com/WordPress-Nexus/wpnexus-jwt-auth
+ * Description: WordPress Nexus JWT Authentication.
+ * Version:     1.0.0
+ * Author:      Always Curious
+ * Author URI:  https://alwayscurious.co
+ * License:     GPL-3.0
+ * License URI: https://oss.ninja/gpl-3.0?organization=Useful%20Team&project=jwt-auth
+ * Text Domain: jwt-auth
+ * Domain Path: /languages
+ *
+ * @package jwt-auth
+ */
+
+defined( 'ABSPATH' ) || die( "Can't access directly" );
